cb63193cf98fe5d367c16b47504cdccefc381c9f
CaMiMtoto <jeanpaulcami@live.com>
2023-11-27 18:39:05 +0200

    add option to download an attachment

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -225,5 +225,17 @@ router.get('/export', async (req, res) => {
     workbook.xlsx.write(res).then(() => res.status(200).end());
 });
 
+router.get('/download/:id', async (req, res) => {
+    const task = await Task.findById(req.params.id);
+    if (!task) {
+        return res.status(404).send();
+    }
+    res.download(task.attachment, (err) => {
+        if (err) {
+            res.status(500).send(err);
+        }
+    });
+});
+
 
 module.exports = router;
